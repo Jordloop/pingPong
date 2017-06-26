3d53a9f822ccac4b46fb84a19c418d062b20e96a
JordLoop <Jordloop@gmail.com>
2017-06-26 14:27:39 -0700

    add gulp-concat and task for gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,26 @@
 var browserify = require('browserify');
 var source = require('vinyl-source-stream');
 var gulp = require('gulp');
+var concat = require('gulp-concat');
+var  uglify = require('gulp-uglify');
+
+gulp.task('concatInterface', function() {
+  //Maintain fileName-interface.js naming convention throughout.
+  return gulp.src(['./js/*-interface.js'])
+    .pipe(concat('allConcat.js'))
+    .pipe(gulp.dest('./tmp'));
+});
 
 //First, we call the browserify function and instruct which files to browserify.
-gulp.task('jsBrowserify', function() {
-  return browserify({ entries: ['./js/pingpong-interface.js'] })
+gulp.task('jsBrowserify', ['concatInterface'], function() {
+  return browserify({ entries: ['./tmp/allConcat.js'] })
     .bundle()
     .pipe(source('app.js'))
     .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task("minifyScripts", ["jsBrowserify"], function(){
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
 });
